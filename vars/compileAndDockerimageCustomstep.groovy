7cee85b7bb822b71a47ff6b276ff494d94c1c8db
Exzelsios <tobiasbohnxz@gmail.com>
2022-04-08 11:30:08 +0200

    define containers in customLib

--- vars/compileAndDockerimageCustomstep.groovy
+++ vars/compileAndDockerimageCustomstep.groovy
@@ -1,19 +1,30 @@
 // vars/buildPlugin.groovy
 def call(String foldername, String dockerTag) {
-  stage('Checkout Git') {
-    git 'https://github.com/NovatecConsulting/technologyconsulting-containerexerciseapp.git'
-  }
-  stage('Build Todo-Backend') {
-  // container('maven') {
-      dir("todobackend") {
-        sh 'mvn clean install'
+  def containers = [
+    containerTemplate(name: 'maven', image: 'maven:3.8.1-jdk-8', command: 'sleep', args: '99d'),
+    containerTemplate(name: 'docker-builder', image: 'buildah/buildah', command: 'sleep', args: '99d', privileged: true)
+  ]
+  
+  def label = "tb-test-${UUID.randomUUID().toString()}"
+
+  podTemplate(label: label, containers: containers) {
+    node(label) {
+      stage('Checkout Git') {
+        git 'https://github.com/NovatecConsulting/technologyconsulting-containerexerciseapp.git'
       }
-  //  }
-  }
-  stage('Build Docker Image Todo-Backend') {
-  //  container('dockerBuilder') {
-      // Wie taggen und wohin pushen?
-      sh 'podman build -f Dockerfile-todobackend -t technologyconsulting-containerexerciseapp-todobackend:jenkinsbuild1 .'
+      stage('Build Todo-Backend') {
+        container('maven') {
+          dir("todobackend") {
+            sh 'mvn clean install'
+          }
+        }
+      }
+      stage('Build Docker Image Todo-Backend') {
+        container('docker-builder') {
+          // Wie taggen und wohin pushen?
+          sh 'podman build -f Dockerfile-todobackend -t technologyconsulting-containerexerciseapp-todobackend:jenkinsbuild1 .'
+        }
+      }
     }
-  //}
+  }
 }
