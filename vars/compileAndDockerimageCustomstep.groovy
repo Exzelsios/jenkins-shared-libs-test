e415991b657d6c94a5cb37d97977bd5c4391b85e
Exzelsios <tobiasbohnxz@gmail.com>
2022-04-08 12:44:08 +0200

    variables in customstep

--- vars/compileAndDockerimageCustomstep.groovy
+++ vars/compileAndDockerimageCustomstep.groovy
@@ -7,14 +7,18 @@ def call(String foldername, String dockerTag) {
   
   def label = "tb-test-${UUID.randomUUID().toString()}"
 
-  podTemplate(label: label, containers: containers) {
+  podTemplate(label: label, containers: containers,
+    volumes: [
+      hostPathVolume(hostPath: '/var/lib/containers', mountPath: '/var/lib/containers')
+    ]
+  ){
     node(label) {
       stage('Checkout Git') {
         git 'https://github.com/NovatecConsulting/technologyconsulting-containerexerciseapp.git'
       }
-      stage('Build Todo-Backend') {
+      stage("Build ${foldername}") {
         container('maven') {
-          dir("todobackend") {
+          dir("${foldername}") {
             sh 'mvn clean install'
           }
         }
@@ -22,7 +26,7 @@ def call(String foldername, String dockerTag) {
       stage('Build Docker Image Todo-Backend') {
         container('docker-builder') {
           // Wie taggen und wohin pushen?
-          sh 'podman build -f Dockerfile-todobackend -t technologyconsulting-containerexerciseapp-todobackend:jenkinsbuild1 .'
+          sh "podman build -f Dockerfile-${foldername} -t technologyconsulting-containerexerciseapp-${foldername}:${dockerTag} ."
         }
       }
     }
