066b81ece3327cf24278a0a5cb051c472700a6d2
Tobias Bohn <tobiasbohnxz@gmail.com>
2022-04-13 13:55:14 +0200

    Remove unused files

--- vars/podTemplates.groovy
+++
@@ -1,4 +0,0 @@
-  def containers = [
-    containerTemplate(name: 'maven', image: 'maven:3.8.1-jdk-8', command: 'sleep', args: '99d'),
-    containerTemplate(name: 'docker-builder', image: 'buildah/buildah', command: 'sleep', args: '99d', privileged: true)
-  ]
